10c46ee2687e39707c8e84a63599f50c0e42a4a8
oliverma0927 <798868909@qq.com>
2025-04-13 22:09:11 +0800

    Add files via upload

---
+++ testMaven/src/main/java/org/example/User.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import lombok.Data;
+
+//@Data
+public class User {
+    String name;
+    String gender;
+    int age;
+//    public User(String name, String gender, int age) {
+//
+//    }
+//
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
